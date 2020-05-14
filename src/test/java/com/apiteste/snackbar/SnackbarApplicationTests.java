03f3c0cb3877a521f8b2e3ae09135f79a8e7518a
Bruno de Souza Rocha <brunodotcom@outlook.com>
2020-05-13 22:02:39 -0300

    Project Start, adding spring boot project scaffold

---
+++ src/test/java/com/apiteste/snackbar/SnackbarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiteste.snackbar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnackbarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
