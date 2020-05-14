abd98c55bebde653f0c2c6c52575783bf9b877b0
Bruno de Souza Rocha <brunodotcom@outlook.com>
2020-05-13 22:15:49 -0300

    Creating Ingridient Model

---
+++ src/main/java/com/apiteste/snackbar/models/Ingridient.java
@@ -0,0 +1,49 @@
+package com.apiteste.snackbar.models;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="ingridients")
+public class Ingridient implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	private String name;
+	
+	private BigDecimal value;
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public BigDecimal getValue() {
+		return value;
+	}
+
+	public void setValue(BigDecimal value) {
+		this.value = value;
+	}
+}
